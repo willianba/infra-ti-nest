76ec3b2e89b3033efccb3b2f8d151354e643af76
Willian Alves <wba.alves@gmail.com>
2019-11-20 00:42:56 -0300

    make app use s2 endpoint to work properly

--- src/image-upload.service.ts
+++ src/image-upload.service.ts
@@ -8,6 +8,7 @@ config();
 const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 const s3 = new AWS.S3();
 AWS.config.update({
+  s3BucketEndpoint: true,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
